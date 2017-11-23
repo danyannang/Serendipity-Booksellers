2d9f910853443fa6703aa5ae075da68f69205643
danyannang <33912123+danyannang@users.noreply.github.com>
2017-11-23 15:11:39 -0800

    Update Inventory.h
    Changed some params

--- Inventory.h
+++ Inventory.h
@@ -14,8 +14,8 @@ public:
 	void menu();
 	std::string bookListing(std::string choice);
 	void initialInventory(std::string **bookData);
-	void addBook();
-	void deleteBook();
-	void editBook(std::string bookChoice);
+	void addBook(std::string **bookData);
+	void deleteBook(std::string **bookData);
+	void editBook(std::string **bookData, std::string bookChoice);
 	enum field { INDEX, ISBN = 0, TITLE, AUTHOR, PUBLISHER, DATE, WHOLESALE, RETAIL, QUANTITY };
 };
