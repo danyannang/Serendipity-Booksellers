6f7e49e9cca300440dee25e88452944a95c02f59
ramgmtk <33914216+ramgmtk@users.noreply.github.com>
2017-11-23 12:16:08 -0800

    Update Cashier.h
    made module destructor virtual for polymorph

--- Cashier.h
+++ Cashier.h
@@ -5,7 +5,7 @@
 class Module {
 public:
 	Module();
-	~Module() { std::cout << "This is a flag to show module has been destroyed\n";  }
+	virtual ~Module() { std::cout << "This is a flag to show module has been destroyed\n";  }
 	std::string bookSearch(struct book *x);
 	std::string titleCompare(struct book *x);
 };
