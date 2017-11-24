572db458317596b4adf4b1c35feb1a370407d8b5
ramgmtk <33914216+ramgmtk@users.noreply.github.com>
2017-11-24 13:33:32 -0800

    Update Cashier.h
    updated h to reflect changes in the cpp

--- Cashier.h
+++ Cashier.h
@@ -3,12 +3,15 @@
 #include <string>
 
 class Module {
+	std::string bookData[100][8];
 public:
 	Module();
 	virtual ~Module() { std::cout << "This is a flag to show module has been destroyed\n";  }
+	enum field { INDEX, ISBN = 0, TITLE, AUTHOR, PUBLISHER, DATE, WHOLESALE, RETAIL, QUANTITY };
+	void createBookArray();
 	int bookSearch(); //(booklist [i][j]
-	std::string titleCompare();
 	virtual void cashierMenu();
+	virtual void inventoryMenu();
 };
 class Cashier : public::Module
 {
@@ -23,4 +26,16 @@ public:
 	void setTotal(double);
 	void setTaxRate();
 	void setSubTotal(int);
+};
+class Inventory : public::Module
+{
+public:
+	Inventory();
+	~Inventory() { std::cout << "This is a flag to show an Inventory has been destroyed.\n"; }
+	void inventoryMenu();
+	//std::string bookListing(std::string choice);
+	//void addBook(std::string **bookData);
+	//void deleteBook(std::string **bookData);
+	//void editBook(std::string **bookData, std::string bookChoice);
+	//void initialInventory(std::string **bookData);
 };
