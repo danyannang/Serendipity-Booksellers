17291223d9ccafa6faa57e800d4f5a83ed319db1
danyannang <33912123+danyannang@users.noreply.github.com>
2017-11-25 08:37:54 -0800

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -54,10 +54,12 @@ std::string InventoryModule::bookListing(std::string choice) //Should have a def
 		std::cin >> bookChoice;
 		return bookChoice; 
 	}
+	return bookChoice;
 }
 
 void InventoryModule::initialInventory(std::string **bookData) //Should only be ran once during first run of inventory module in a new initialization of program, a new file will be created after. This will create the 2d array of books.
 {
+	//REPLACE WITH CODE FROM FATHER MODULE
 	std::cout << "initialInventory" << std::endl;
 	std::string unsplit;
 	std::ifstream iniBooks("BookStock.txt");
@@ -72,21 +74,27 @@ void InventoryModule::initialInventory(std::string **bookData) //Should only be 
 	{
 		std::cout << "I'm open" << std::endl;
 	}
+	if (!iniBooks)
+	{
+		std::cout << "Error" << std::endl;
+	}
+	system("Pause");
 
+	int nthBook = 0;
 	while (std::getline(iniBooks, unsplit, '\t'))
 	{
 		std::cout << "good";
 		bool exists = false;//To be set to true if the current book already exists in the data
-		int nthBook = 0;
 
 		for (int attr = 0; attr < 8; attr++)//Cycle through the attributes for the book, will just keep reading if exists == true
 		{
 			if (attr == 0)//If it's the first attribute of a book, check if one already exists in the system
 			{
-				for (int i = 0; i < 8; i++)
+				for (int i = 0; i < nthBook+1; i++)
 				{
-					if (unsplit == bookData[i][1])//If the read in ISBN is the exact same as another, then increment the quantity attribute by one
+					if (unsplit == bookData[i][0])//If the read in ISBN is the exact same as another, then increment the quantity attribute by one
 					{
+						std::cout << "Yes";
 						int addOneTo = stoi(bookData[i][QUANTITY]);
 						bookData[i][QUANTITY] = std::to_string(addOneTo++);
 						exists = true; 
@@ -111,24 +119,26 @@ void InventoryModule::initialInventory(std::string **bookData) //Should only be 
 			std::getline(iniBooks, unsplit, '\t');
 		}
 
-		nthBook++; 
-
-		//Dynamically allocate a new 2d array with one extra space for a book, then copy the contents of the original array over. (Like how a vector keeps increasing in size)
-		int newSize = nthBook++; 
-		std::string **nBookData = new std::string*[newSize];
-		for (int i = 0; i < newSize; i++)
+		if (exists == false)
 		{
-			nBookData[i] = new std::string[8];
-		}
-		for (int i = 0; i < newSize--; i++)
-		{
-			for (int j = 0; i < 8; j++)
+			nthBook++;
+			//Dynamically allocate a new 2d array with one extra space for a book, then copy the contents of the original array over. (Like how a vector keeps increasing in size)
+			int newSize = nthBook;
+			std::string **nBookData = new std::string*[newSize];
+			for (int i = 0; i < newSize; i++)
 			{
-				nBookData[i][j] = bookData[i][j];
+				nBookData[i] = new std::string[8];
 			}
+			for (int i = 0; i < newSize--; i++)
+			{
+				for (int j = 0; i < 8; j++)
+				{
+					nBookData[i][j] = bookData[i][j];
+				}
+			}
+			delete[] bookData;
+			bookData = nBookData;
 		}
-		delete[] bookData;
-		bookData = nBookData;
 	}
 
 	iniBooks.close();
@@ -138,6 +148,9 @@ void InventoryModule::initialInventory(std::string **bookData) //Should only be 
 void InventoryModule::addBook(std::string **bookData)//Adds the info for a new book at the end of the inventory(add condition if book equals exist to just add one to quantity)
 {
 	std::string newBook[8];
+	std::cout << "Enter info for new book: " << endl;
+	std::cout << "ISBN: "; 
+	std::cin >> newBook[ISBN]; 
 }
 
 void InventoryModule::deleteBook(std::string **bookData)//Deletes all info for a particular book, deletes the space for it and shrinks array
