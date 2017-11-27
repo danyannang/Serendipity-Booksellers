1f4e534215d1a7d7da6ed22319589775038da058
danyannang <33912123+danyannang@users.noreply.github.com>
2017-11-26 20:09:08 -0800

    Update RunInventory.cpp

--- RunInventory.cpp
+++ RunInventory.cpp
@@ -4,21 +4,12 @@ using namespace std;
 
 int main()
 {
-	//Not sure if this is really needed
-	struct Book
-	{
-		string isbn;
-		string title;
-		string author;
-		string publisher;
-		string date;
-		string wholesale;
-		string retail;
-		string quantity;
-	};
 
-	 //First index is just a spot for the index of the book in the big list of books, second index value is equivalent to the info of the book
-	int newSize = 1;
+	//To track array length
+	int invenSize = 2;
+
+	//First index is just a spot for the index of the book in the big list of books, second index value is equivalent to the info of the book
+	int newSize = 2;
 	std::string **bookData = new std::string*[newSize];
 	for (int i = 0; i < newSize; i++)
 	{
@@ -26,7 +17,43 @@ int main()
 	}
 
 	InventoryModule inven; 
-	inven.initialInventory(bookData); 
+	inven.initialInventory(bookData, invenSize);
+
+	string choice = inven.menu(); 
+
+	if (choice != "4")
+	{
+		while (choice != "4")
+		{
+			if (choice == "1")
+			{
+				inven.addBook(bookData, invenSize);
+
+				choice = inven.menu();
+			}
+			else if (choice == "2")
+			{
+				inven.bookListing(choice, bookData, invenSize);
+				inven.deleteBook(bookData, invenSize);
+
+				choice = inven.menu();
+			}
+			else if (choice == "3")
+			{
+				string bookChoice = inven.bookListing(choice, bookData, invenSize);
+				if (bookChoice != "Q")
+				{
+					inven.editBook(bookData, bookChoice);
+				}
+
+				choice = inven.menu();
+			}
+		}
+	}
+	else if (choice == "4")
+	{
+		//Code to go back to main Module's menu method
+	}
 
 	system("Pause");
 	return 0;
